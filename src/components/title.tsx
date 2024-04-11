d8c8ac3e88099fc31c87187f792baaf991fbeca9
matheusscode <matheussdev3@gmail.com>
2024-04-11 19:45:29 -0400

    feat: adds title component

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,10 +1,11 @@
+import { cn } from "@/lib/utils"
 import { ComponentProps, FC } from "react"
 
 type TitleProps = ComponentProps<"h1">
 
-const Title: FC<TitleProps> = ({ children, ...props }) => {
+const Title: FC<TitleProps> = ({ children, className, ...props }) => {
     return (
-        <h1 {...props} className="text-3xl font-bold tracking-tight font-sans">
+        <h1 {...props} className={cn("text-3xl font-bold tracking-tight font-sans", className)}>
             {children}
         </h1>
     )
