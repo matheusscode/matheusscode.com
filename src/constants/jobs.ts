2ad1918addc90033fa9dd778a8e2a764ffad91a0
matheusscode <matheussdev3@gmail.com>
2024-04-14 06:20:50 -0400

    feaT: add jobs constants

---
+++ src/constants/jobs.ts
@@ -0,0 +1,23 @@
+export const jobs = [
+  {
+    id: 1,
+    company: "Ream",
+    office: "Engenheiro de Software Júnior",
+    beginning_and_end: "Atual",
+    route: "",
+  },
+  {
+    id: 2,
+    company: "Hospital Adventista de Manaus",
+    office: " A.D.S & Desenvolvedor FullStack",
+    beginning_and_end: "17/04/23 à 23/07/2024",
+    route: "",
+  },
+  {
+    id: 3,
+    company: "Trixx - Tecnologia e Marketing",
+    office: "Desenvolvedor Mobile Júnior",
+    beginning_and_end: "17/04/23 à 23/07/2024",
+    route: "",
+  },
+];
